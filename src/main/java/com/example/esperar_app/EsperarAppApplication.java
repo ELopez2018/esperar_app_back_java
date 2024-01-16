a614f44dd7248687f4ad1ec5173458b1fecd7c92
juancalderonx <juanescalderon12@gmail.com>
2024-01-16 09:37:04 -0500

    feat(): initial commit

---
+++ src/main/java/com/example/esperar_app/EsperarAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.esperar_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsperarAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsperarAppApplication.class, args);
+    }
+
+}
