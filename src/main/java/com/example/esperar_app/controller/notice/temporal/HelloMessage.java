f628dce0aa8cce92b6c542d8d82e251fe1e4121d
juancalderonx <juanescalderon12@gmail.com>
2024-01-31 12:10:04 -0500

    feat(websocket): news services in websocket finished

--- src/main/java/com/example/esperar_app/controller/notice/temporal/HelloMessage.java
+++
@@ -1,9 +0,0 @@
-package com.example.esperar_app.controller.notice.temporal;
-
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter @Setter
-public class HelloMessage {
-    private String name;
-}
