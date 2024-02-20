c2da84cc548929ba2b3e014f5982b20457533b13
Esteban Dev <juanescalderon12@gmail.com>
2024-02-19 21:07:58 -0500

    refactor(users): refactoring users and vehicles functionalities

--- src/main/java/com/example/esperar_app/persistence/repository/VehicleRepository.java
+++ src/main/java/com/example/esperar_app/persistence/repository/VehicleRepository.java
@@ -3,6 +3,14 @@ package com.example.esperar_app.persistence.repository;
 import com.example.esperar_app.persistence.entity.vehicle.Vehicle;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
+import java.util.Date;
+import java.util.List;
 
+public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
+    List<Vehicle> findBySoatExpirationDateBetweenOrTecnoMechanicalExpirationDateBetween(
+            Date soatExpirationDate,
+            Date soatExpirationDate2,
+            Date tecnoMechanicalExpirationDate,
+            Date tecnoMechanicalExpirationDate2
+    );
 }
