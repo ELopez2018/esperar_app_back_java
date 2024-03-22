e82df19c60e7c73ada3ed359d031216589801e95
Esteban Dev <juanescalderon12@gmail.com>
2024-03-22 17:34:47 -0500

    refactor(): refactoring any endpoints

---
+++ src/main/java/com/example/esperar_app/persistence/dto/user/Color.java
@@ -0,0 +1,12 @@
+package com.example.esperar_app.persistence.dto.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+public class Color {
+    private String name;
+    private String value;
+}
